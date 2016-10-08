891dbd03ce7ec7370e1633f19744c3dc78bc5fcb
Detective Squirrel <ratman7539@windowslive.com>
2016-10-08 23:39:08 +1300

    Add files via upload

---
+++ WindowsFormsApplication2/bin/Debug/Batch%20Files/Start.bat
@@ -0,0 +1,4 @@
+cd /d "C:\Program Files (x86)\Grinding Gear Games\Path of Exile\"
+start PathOfExile.exe --nosound --noasync -gc 2
+cd /d "Z:\RoboBuddy\"
+start Robo.exe --autostart
---
+++ WindowsFormsApplication2/bin/Debug/Batch%20Files/Stop.bat
@@ -0,0 +1,2 @@
+taskkill /f /im "PathOfExile.exe" >nul
+taskkill /IM cmd.exe
